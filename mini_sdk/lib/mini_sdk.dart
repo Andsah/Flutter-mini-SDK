a71ee79bd0f0a4fb9cb7dfd4d11494352d7e887e
Andsah <adde11__@live.se>
2023-03-28 23:17:09 +0200

    hopefully I can make a dummy app to test this later.

--- mini_sdk/lib/mini_sdk.dart
+++ mini_sdk/lib/mini_sdk.dart
@@ -1,7 +1,30 @@
 library mini_sdk;
 
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
+}
+
+/*
+Product page class, fulfills the design pattern found at 
+-> https://ui-patterns.com/patterns/ProductPage <-
+ */
+class ProductPage extends StatefulWidget {
+  const ProductPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => ProductPageState();
+}
+
+class ProductPageState extends State<ProductPage> {
+  @override
+  Widget build(BuildContext context) {
+    // maybe some stuff here who knows
+
+    return Container(child: Column(children: []));
+  }
 }
